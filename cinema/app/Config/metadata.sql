ffe379f3eb56dcfa11d972270357d5041477094e
andreymasiero <andreymasiero96@gmail.com>
2024-12-04 15:33:12 -0300

    first

--- cinema/app/Config/metadata.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE 'filmes' (
-    'id' int(11) NOT NULL AUTO_INCREMENT,
-    'nome' varchar(100) DEFAULT NULL,
-    'ano'' int(11) DEFAULT NULL,
-    'duracao' varchar(5) DEFAULT NULL,
-    'idioma' varchar(50) DEFAULT NULL,
-    PRIMARY KEY('id')
-    ) ENGINE=innoDB DEFAULT CHARSET=utf8;
--- cinema/app/Model/Behavior/FilmeTest.php
+++
@@ -1,5 +0,0 @@
-<?php
-class FilmeTest extends  CakeTestCase {
-
-}
-?>
